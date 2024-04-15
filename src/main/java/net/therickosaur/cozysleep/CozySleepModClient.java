504ed3b1363de0a9b10de9250a7aa7213b941fd8
Rick Kralt <r.kralt99@gmail.com>
2024-04-15 11:12:42 +0200

    Initial commit

---
+++ src/main/java/net/therickosaur/cozysleep/CozySleepModClient.java
@@ -0,0 +1,10 @@
+package net.therickosaur.cozysleep;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CozySleepModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/therickosaur/cozysleep/CozySleepModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.therickosaur.cozysleep;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CozySleepModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
